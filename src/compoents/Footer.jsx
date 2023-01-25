7a8303c9efb6eab25bffbbae48daddb51601d5a7
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2023-01-25 13:07:13 +0200

    eyal sberro

--- src/compoents/Footer.jsx
+++ src/compoents/Footer.jsx
@@ -13,6 +13,7 @@ export default function Footer() {
         <div className='footercomp'>
             <div className='footersection'>
                 <img className='junexlogo' src="https://i.ibb.co/fvVG4S8/Whats-App-Image-2022-05-22-at-5-07-48-PM-removebg-preview.png" alt='junexlogo'></img>
+                <p className='rights'><a href='https://www.linkedin.com/in/eyalsberro' target='_blank'>Built By Eyal Sberro</a></p>
             </div>
             <hr></hr>
             <p className='rights'>כל הזכויות שמורות ©</p>
